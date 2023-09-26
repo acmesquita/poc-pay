f7a4cc03b5c79c8c40918ca5b1fda160eae4d779
Catharina Mesquita <catha.ana.1994@gmail.com>
2023-09-26 10:00:24 -0300

    refactor: change folder names

--- config/zeitwerk.rb
+++ config/zeitwerk.rb
@@ -4,10 +4,10 @@ require 'zeitwerk'
 
 loader = Zeitwerk::Loader.new
 loader.push_dir('app/')
-loader.push_dir('app/domain/models')
-loader.push_dir('app/domain/errors')
-loader.push_dir('app/domain/repositories')
-loader.push_dir('app/usecases')
+loader.push_dir('app/core/models')
+loader.push_dir('app/core/errors')
+loader.push_dir('app/core/repositories')
+loader.push_dir('app/core/usecases')
 loader.push_dir('app/infra/controllers')
 loader.push_dir('app/infra/serializers')
 loader.push_dir('lib')
